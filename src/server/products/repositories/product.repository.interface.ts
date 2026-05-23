446b6a1524021299b2a1b301c956be9ebc03c518
afcodenet-debug <afcodenet@gmail.com>
2026-05-23 14:05:03 +0200

    Update product.repository.interface.ts

--- src/server/products/repositories/product.repository.interface.ts
+++ src/server/products/repositories/product.repository.interface.ts
@@ -1,14 +1,17 @@
-// src/server/products/repositories/product.repository.interface.ts
-import { ProductEntity } from '../types/product.types';
-import { CreateProductDTO, UpdateProductDTO, ProductListQuery } from '../dtos/product.dto';
-import { PaginatedResult } from '../../types/common.types';
-import { IProductSyncContract } from './product.sync.contract';
+export interface ProductEntity {
+  id: string | number;
+  business_id?: string;
+  name: string;
+  description?: string | null;
+  price: number;
+  category_id?: number | string | null;
+  image_url?: string | null;
+  is_available: boolean;
+  stock_quantity?: number;
+  unit?: string | null;
+  low_stock_threshold?: number;
+}
 
-export interface IProductRepository extends IProductSyncContract {
-  findById(id: string, businessId: string): Promise<ProductEntity | null>;
-  findAll(businessId: string, query: ProductListQuery): Promise<PaginatedResult<ProductEntity>>;
-  create(data: CreateProductDTO, businessId: string, createdBy?: string): Promise<ProductEntity>;
-  update(id: string, data: UpdateProductDTO, businessId: string): Promise<ProductEntity>;
-  softDelete(id: string, businessId: string): Promise<void>;
-  findAvailableForMenu(businessId: string, categoryId?: string): Promise<ProductEntity[]>;
+export interface IProductRepository {
+  findAvailableForMenu(businessId: string): Promise<ProductEntity[]>;
 }
